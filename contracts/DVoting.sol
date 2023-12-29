b1799556a20d9e269a283eabbb968da006f42201
washucode <estherw8525@gmail.com>
2023-12-29 23:13:37 +0300

    add poll related functions

--- contracts/DVoting.sol
+++ contracts/DVoting.sol
@@ -4,7 +4,7 @@ pragma solidity >=0.7.0 <0.9.0;
 
 contract DVoting {
     // content of polls created
-    struct pollsStruct {
+    struct PollsStruct {
         uint id;
         string image;
         string title;
@@ -27,4 +27,115 @@ contract DVoting {
         uint votes;
         address[] voters; 
     }
+
+    // track the number of polls created
+    uint pollsCount;
+
+    // track the number of contestants/ users created
+    uint usersCount;
+    PollsStruct[] polls;
+
+    // map the address of the user to the user struct
+    mapping(address => VoterStruct) public users;
+    // see if user has voted
+    mapping(uint => mapping(address => bool)) voted;
+    // see if user has been added to a poll/has contested
+    mapping(uint => mapping(address => bool)) constanted;
+    // map contestants of a poll
+    mapping(uint => VoterStruct[])  contestants;
+    // check if poll exists
+    mapping(uint => bool)  pollExists;
+
+
+
+    // create event for voted
+    event Voted(
+        string fullname, 
+        address indexed voter, 
+        uint timestamp
+        
+        );
+
+    modifier userOnly{
+        require(users[msg.sender].voter == msg.sender, "You are not a user");
+        _;
+    }
+
+    // create poll
+
+    function createPoll(
+        string memory image,
+        string memory title,
+        string memory description,
+        uint startsAt,
+        uint endsAt
+        ) public userOnly {
+            require(bytes(title).length > 0, "Title is required");
+            require(bytes(description).length > 0, "Description is required");
+            require(startsAt > 0 && endsAt > startsAt, "End date must be greater than start date");
+            require(endsAt > 0, "End time is required");
+
+            PollsStruct memory poll;
+            poll.id = pollsCount++;
+            poll.image = image;
+            poll.title = title;
+            poll.description = description;
+            poll.startsAt = startsAt;
+            poll.endsAt = endsAt;
+            poll.timestamp = block.timestamp;
+            poll.director = msg.sender;
+
+            polls.push(poll);
+            pollExists[poll.id] = true;
+    }
+
+    // update poll
+    function updatePoll(
+        uint id,
+        string memory image,
+        string memory title,
+        string memory description,
+        uint startsAt,
+        uint endsAt
+        ) public userOnly {
+            require(pollExists[id], "Poll does not exist");
+            require(polls[id].director == msg.sender, "You are not the director of this poll");
+            require(bytes(title).length > 0, "Title is required");
+            require(bytes(description).length > 0, "Description is required");
+            require(startsAt > 0 && endsAt > startsAt, "End date must be greater than start date");
+            require(endsAt > 0, "End time is required");
+
+            polls[id].image = image;
+            polls[id].title = title;
+            polls[id].description = description;
+            polls[id].startsAt = startsAt;
+            polls[id].endsAt = endsAt;
+            
+    }
+
+    // delete poll
+    function deletePoll(uint id) public userOnly {
+        require(pollExists[id], "Poll does not exist");
+        require(polls[id].director == msg.sender, "You are not the director of this poll");
+        polls[id].deleted = true;
+    }
+
+    // get poll
+
+    function getPoll(uint id) public view returns(
+        PollsStruct memory 
+        ) {
+            return polls[id];
+    }
+
+    // get all polls
+
+    function getAllPolls() public view returns(
+        PollsStruct[] memory 
+        ) {
+            return polls;
+    }
+
+    // add contestant
+
 }
