2994d61e082025aef64aa5e86f3d428869649012
washucode <estherw8525@gmail.com>
2024-01-02 18:36:31 +0300

    add fvoting , user and contestant functionality

--- src/Blockchain.services.jsx
+++ src/Blockchain.services.jsx
@@ -3,6 +3,7 @@ import address from './abis/address.json'
 import { getGlobalState, setGlobalState } from './store'
 import { ethers } from 'ethers'
 import { isAuthenticated, logoutChat } from './Chat.services'
+import { list } from 'postcss'
 
 
 const { ethereum } = window 
@@ -187,4 +188,141 @@ const deletePoll = async (id) => {
             console.log(error)
             reportError(error)
         }
+}
+
+// structured contestants
+
+const structuredContestants = (contestants, connectedAccount) => 
+    contestants.map((contestant) => ({
+        id: contestant.id.toNumber(),
+        name: contestant.name,
+        image: contestant.image,
+        voter: contestant.voter?.toLowerCase(),
+        voters: contestant.votes.map((vote) => vote.toLowerCase()),
+        votes: contestant.votes.toNumber(),
+    })).sort((x,y) => y.votes - x.votes)
+
+
+// report error
+
+const reportError = (error) => {
+    console.log(error.message)
+    throw new Error("No ethereum object")
+}
+
+// register user
+
+const registerUser = async ({fullname, image}) => {
+    try{
+        if(!ethereum){
+            alert('Please install MetaMask')
+            return
+        }
+        const connectedAccount = getGlobalState('connectedAccount')
+        const contract = getContract()
+        await contract.registerUser(fullname, image, {
+            from: connectedAccount,
+        })
+        await getUser()
+    } catch (error){
+        console.log(error)
+        reportError(error)
+    }
+}
+
+
+// get user
+
+const getUser = async () => {
+    try{
+        if(!ethereum){
+            alert('Please install MetaMask')
+            return
+        }
+        const connectedAccount = getGlobalState('connectedAccount')
+        const contract = getContract()
+        const user = await contract.users(connectedAccount)
+        setGlobalState('user', structuredUser(user))
+    } catch (error){
+        console.log(error)
+        reportError(error)
+    }
+}
+
+
+// contest
+
+const contest = async (id) => {
+    try {
+        if(!ethereum){
+            alert('Please install MetaMask')
+            return
+        }
+        const connectedAccount = getGlobalState('connectedAccount')
+        const contract = getContract()
+        await contract.contest(id, {
+            from: connectedAccount,
+        })
+        await getPoll(id)
+    } catch (error){
+        console.log(error)
+        reportError(error)
+    }
+}
+
+
+// vote
+
+const vote = async (id, cid) => {
+    try {
+        if(!ethereum){
+            alert('Please install MetaMask')
+            return
+        }
+        const connectedAccount = getGlobalState('connectedAccount')
+        const contract = getContract()
+        await contract.vote(id, cid, {
+            from: connectedAccount,
+        })
+        await getPoll(id)
+        await listContestants(id)
+    } catch (error){
+        console.log(error)
+        reportError(error)
+    }
+}
+
+
+
+// list contestants
+
+const listContestants = async (id) => {
+    try {
+        if(!ethereum){
+            alert('Please install MetaMask')
+            return
+        }
+        const contract = getContract()
+        const contestants = await contract.listContestants(id)
+        setGlobalState('contestants', structuredContestants(contestants))
+    } catch (error){
+        console.log(error)
+        reportError(error)
+    }
+}
+
+
+export{
+    isWalletConnected,
+    connectWallet,
+    getPoll,
+    getAllPolls,
+    createPoll,
+    updatePoll,
+    deletePoll,
+    registerUser,
+    getUser,
+    contest,
+    vote,
+    listContestants,
 }
